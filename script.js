a1a6c365e77f3e6599405669dec46580806457b0
Franklin <Franklinau2@gmail.com>
2022-11-13 11:42:35 -0800

    Add place symbol function on players module

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
-const gameBoard = (function () {
-  let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const gameBoard = (function() {
+  let board = ['_', '_', '_', '_', '_', '_', '_', '_', '_',]
 
+  //this module should check if the player selected space is valid
+  //the function that checks if a space is valid should be private
   return {
     board
   }
 })()
 
-const displayController = (function () {
+const players = (symbol) => {
   //cache DOM
-  let $board = document.querySelector('.board')
   let $spaces = document.querySelectorAll('.space')
 
   //bind events
 
-  function _render() {
+
+  function placeSymbol() {
     $spaces.forEach((element, index) => {
-      if (gameBoard.board[index] === 'X' || gameBoard.board[index] === 'O') {
-        element.innerHTML = gameBoard.board[index]
-      } else {
-        element.innerHTML = '_'
-      }
+      element.addEventListener('click', function() {
+        gameBoard.board[index] = symbol
+        displayController.render()
+      })
     })
   }
 
-  _render()
+  return {
+    placeSymbol
+  }
+}
+
+const game = (function() {
+  let playerOne = players('X')
+  let playerTwo = players('O')
+  let gameFinished = false
+  let activePlayer = playerOne
+  activePlayer.placeSymbol()
+
+  function _game() {
+    /*while (gameFinished === false) {
+      playerOne.placeSymbol()
+    }*/
+    //while loop 
+      //activePlayer = playerOne
+      //playerOne.placeSymbol()
+      //checks if it's a win through gameBoard module
+      //activePlayer = playerTwo
+      //playerTwo.placeSymbol()
+      //checks if it's a win
+    //end while loop
+  }
+
+  _game()
+})()
+
+const displayController = (function() {
+  //cache DOM
+  let $spaces = document.querySelectorAll('.space')
+
+  //bind events
+  /*$spaces.forEach((element, index) => {
+    element.addEventListener('click', function() {
+      gameBoard.board[index] = 'X'
+      _render()
+    })
+  })*/
+
+  function render() {
+    $spaces.forEach((element, index) => {
+      element.innerHTML = gameBoard.board[index]
+    })
+  }
+
+  render()
+
+  return {
+    render
+  }
 })()
