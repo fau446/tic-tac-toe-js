ebad0ee22092b654454a8f215ff15239113e7b15
Franklin <Franklinau2@gmail.com>
2022-11-15 11:43:04 -0800

    Add method to check if game has been won

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
 const gameBoard = (function() {
-  let board = ['_', '_', '_', '_', '_', '_', '_', '_', '_',]
+  let board = ['_', '_', '_', '_', '_', '_', '_', '_', '_'] //['0', '1', '2', '3', '4', '5', '6', '7', '8']
 
-  
+  function _arrayEquals(a, b) {
+    return a.every((val, index) => val === b[index])
+  }
+
+  function _winCheck(symbol) {
+    let winStatus = false
+
+    const winConditions = [[board[0], board[1], board[2]], [board[3], board[4], board[5]], 
+    [board[6], board[7], board[8]], [board[0], board[3], board[6]],
+    [board[1], board[4], board[7]], [board[2], board[5], board[8]],
+    [board[0], board[4], board[8]], [board[2], board[4], board[6]] ]
+    winConditions.forEach((element) => {
+      if (_arrayEquals(element, [symbol, symbol, symbol])) {
+        winStatus = true
+      }
+    }) 
+
+    return winStatus
+  }
 
   function validMoveCheck(space) {
     if (space.innerHTML === '_') return true
     return false
+  } 
+
+  function gameStatusCheck(symbol) {
+    //Check if it's a win first
+    if (_winCheck(symbol)) {
+      return "win"
+    }
+    //If not a win and board is full, it is a tie
+    //otherwise, the game goes on. return false
   }
 
   return {
     board,
-    validMoveCheck
+    validMoveCheck,
+    gameStatusCheck
   }
 })()
 
 const players = (symbol) => {
   //cache DOM
-  let $spaces = document.querySelectorAll('.space')
 
   //bind events
 
@@ -27,6 +54,7 @@ const players = (symbol) => {
   }
 
   return {
+    symbol,
     placeSymbol
   }
 }
@@ -43,7 +71,6 @@ const game = (function() {
   //bind events
   $spaces.forEach((element, index) => {
     element.addEventListener('click', function() {
-      console.log(gameBoard.validMoveCheck(element))
       if (gameBoard.validMoveCheck(element)) {
         _game(index)
       }
@@ -53,6 +80,7 @@ const game = (function() {
   function _game(index) {
     activePlayer != playerOne ? activePlayer = playerOne : activePlayer = playerTwo
     activePlayer.placeSymbol(index)
+    let gameStatus = gameBoard.gameStatusCheck(activePlayer.symbol)
   }
 
 
